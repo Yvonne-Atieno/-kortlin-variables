bb5d1c2dc8f4938cb43b14a00d8af101ba7f0c58
NjeriMarion <njeriwmarion@gmail.com>
2023-04-01 09:13:20 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+fun main(){
+    var num=5
+    println(num)
+
+
+    var name="yvonne"
+    println(name)
+
+    var cat="white"
+    println(cat)
+
+    var car="black"
+    println(car)
+
+    var girl="beautiful"
+    println(girl)
+
+    var num1=6
+    var num2=5
+    var sum=num1+num2
+    println(sum)
+
+    var numb1 = 90
+    var numb2 =14
+    var difference=numb1-numb2
+    println(difference)
+
+    var numbe1=13
+    var numbe2=16
+    var product=numbe1*numbe2
+    println(product)
+
+    var number1=17
+    var number2=3
+    var result=number1/number2
+    println(result)
+
+    var numbers1=25
+    var numbers2=2
+    var remainder=numbers1%numbers2
+    println(remainder)
+
+}
